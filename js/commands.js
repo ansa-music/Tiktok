6db246b85c47840a0987a45589689550e27a4c83
ansa-music <nurpro2005@gmail.com>
2026-04-07 21:14:02 +0500

    Update commands.js

--- js/commands.js
+++ js/commands.js
@@ -1,20 +1,13 @@
-const CISCO_DB = {
-    basics: {
-        title: "Базовая настройка роутера",
-        steps: [
-            "Подключите консольный кабель к PC и Router",
-            "Откройте вкладку Desktop -> Terminal в Packet Tracer",
-            "Введите следующие команды:"
-        ],
-        code: "enable\nconf t\nhostname R1\nno ip domain-lookup"
-    },
-    vlan: {
-        title: "Настройка VLAN (Виртуальные сети)",
-        steps: [
-            "Зайдите в Switch",
-            "Создайте номер VLAN",
-            "Назначьте порты в этот VLAN"
-        ],
-        code: "vlan 10\nname STAFF\nexit\nint range fa0/1-10\nswitchport mode access\nswitchport access vlan 10"
-    }
-};
+// Добавь это в объект CISCO_DB
+lab12: {
+    title: "№12 Зертханалық жұмыс: OSPF",
+    subtitle: "Динамикалық маршруттау (OSPF протоколы)",
+    steps: [
+        [span_3](start_span)"1. Собери топологию: 3 роутера 2911 и 3 PC[span_3](end_span).",
+        "2. [span_4](start_span)[span_5](start_span)Соедини Router2(Gi0/0) с Router1(Gi0/0) и Router2(Gi0/1) с Router3(Gi0/1)[span_4](end_span)[span_5](end_span).",
+        "3. [span_6](start_span)[span_7](start_span)Настрой IP-адреса на всех интерфейсах согласно схеме[span_6](end_span)[span_7](end_span).",
+        "4. [span_8](start_span)Включи OSPF на Router2: укажи сети 10.10.10.0/30, 10.10.11.0/30 и 192.168.1.0/24[span_8](end_span).",
+        "5. [span_9](start_span)Проверь связность командой 'show ip ospf neighbor'[span_9](end_span)."
+    ],
+    code: "! Настройка Router2\nenable\nconf t\nrouter ospf 1\n network 192.168.1.0 0.0.0.255 area 0\n network 10.10.10.0 0.0.0.3 area 0\n network 10.10.11.0 0.0.0.3 area 0\nexit"
+}
